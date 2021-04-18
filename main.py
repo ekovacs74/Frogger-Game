290595bb9843c37e6b81b1007ed8459f05a6ad2e
NokiNeki <64992246+NokiNeki@users.noreply.github.com>
2021-04-18 02:49:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,52 +12,68 @@ DarkGreen2 = (90, 135, 62)
 
 
 '''Chars'''
-Frogger_Image = pygame.image.load(os.path.join('Assets', 'Frogger.png'))
-FroggerX, FroggerY, FroggerWidth, FroggerHeight = 250, 250, 64, 64
+Frogger_Image = pygame.image.load(os.path.join('Assets', 'Frogger.png')) # Loading image for Frogger
+FroggerX, FroggerY, FroggerWidth, FroggerHeight = 250, 250, 64, 64 # Properties of Frogger
+Frogger = pygame.transform.scale(Frogger_Image, (FroggerWidth, FroggerHeight)) # Sacling Frogger
 
-Frogger = pygame.transform.scale(Frogger_Image, (FroggerWidth, FroggerHeight))
+Bus_Image = pygame.image.load(os.path.join('Assets', 'Schoolbus.png')) # Loading image for School Bus
+BusRight = pygame.transform.flip(Bus_Image, True, False) # Flips bus so it faces to the right
+BusLeft = pygame.transform.flip(Bus_Image, False, False) # Flip bus so it to the left
 
 
-'''Screen Update Speed'''
-FPS = 60
+FPS = 60 # Screen update speed
 
 def drawScreen():
-    WIN.fill(DarkGreen2)
+    WIN.fill(DarkGreen2) # Background
 
+    # For things on the screen (Might make a separate function)
     WIN.blit(Frogger, (FroggerX, FroggerY))
 
-    pygame.display.update()
+    pygame.display.update() # Updates the screen
 
 def main():
     global FroggerY, FroggerX
     clock = pygame.time.Clock()
-    truth = True
-    while truth == True:
+    GameLoop = True # Variable for the Main Game Loop
+
+    ScreenMoveUp = 0
+    
+    while GameLoop == True: # Main Game Loop
+        
         clock.tick(FPS)
+        
         for event in pygame.event.get():
-              if event.type == pygame.QUIT:  # Usually wise to be able to close your program.
+              
+              if event.type == pygame.QUIT:  # Quit Event
                   truth = False
-              elif event.type == pygame.KEYDOWN:
-                  if event.key == pygame.K_UP and FroggerY > 0:
+              
+              elif event.type == pygame.KEYDOWN: # Buttons Pressed
+                  
+                  if event.key == pygame.K_UP and FroggerY > 0: # Button for moving up with limits
                       FroggerY -= 64
-                  elif event.key == pygame.K_LEFT and FroggerX > 0:
+                  elif event.key == pygame.K_LEFT and FroggerX > 0: # Button for moving left with limits
                       FroggerX -= 64
-                  elif event.key == pygame.K_DOWN and FroggerY < ScreenHeight - FroggerHeight * 2:
+                  elif event.key == pygame.K_DOWN and FroggerY < ScreenHeight - FroggerHeight * 2: # Button for moving down with limits
                       FroggerY += 64
-                  elif event.key == pygame.K_RIGHT and FroggerX < ScreenWidth - FroggerWidth * 2:
+                  elif event.key == pygame.K_RIGHT and FroggerX < ScreenWidth - FroggerWidth * 2: # Button for moving right with limits
                       FroggerX += 64
 
-        drawScreen()
+        drawScreen() # Draws what needs to be drawn for the game
 
-        FroggerY += 1
+        ScreenMoveUp += 1
+        if ScreenMoveUp == 10: # Slowly moves screen elements down.
+            
+            FroggerY += 1
+            
+            ScreenMoveUp = 0
 
-        if FroggerY >= ScreenHeight:
+        if FroggerY >= ScreenHeight: # If frogger gets below the bottom of the screen
             print("Game Over")
-            truth = False
+            GameLoop = False
 
 
-    pygame.quit()
+    pygame.quit() # Stops pygame once main game loop is over
 
 
-
-main()
+if __name__ == "__main__":
+    main() # Runs the main function
