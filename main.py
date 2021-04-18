6d10055f4640e852e6b91628949042f0a19f83d8
ekovacs74 <82689769+ekovacs74@users.noreply.github.com>
2021-04-18 01:19:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,63 @@
 import pygame
+import os
 
-
-ScreenWidth, ScreenHeight = 500, 500
+ScreenWidth, ScreenHeight = 704, 704
 WIN = pygame.display.set_mode((ScreenWidth, ScreenHeight))
+pygame.display.set_caption("Frogger Game")
 
 '''Colors'''
 Green = (76, 163, 54)
 DarkGreen = (55, 115, 40)
+DarkGreen2 = (90, 135, 62)
 
 
 '''Chars'''
-FroggerX, FroggerY, FroggerWidth, FroggerHeight = 250, 250, 16, 16
-Frogger = pygame.Rect(FroggerX, FroggerY, FroggerWidth, FroggerHeight)
+Frogger_Image = pygame.image.load(os.path.join('Assets', 'Frogger.png'))
+FroggerX, FroggerY, FroggerWidth, FroggerHeight = 250, 250, 64, 64
+
+Frogger = pygame.transform.scale(Frogger_Image, (FroggerWidth, FroggerHeight))
+
+
+'''Screen Update Speed'''
+FPS = 60
 
 def drawScreen():
-    WIN.fill(Green)
+    WIN.fill(DarkGreen2)
 
-    pygame.draw.rect(WIN, DarkGreen, Frogger)    
+    WIN.blit(Frogger, (FroggerX, FroggerY))
+
     pygame.display.update()
 
 def main():
+    global FroggerY, FroggerX
+    clock = pygame.time.Clock()
     truth = True
     while truth == True:
-       for event in pygame.event.get():
+        clock.tick(FPS)
+        for event in pygame.event.get():
               if event.type == pygame.QUIT:  # Usually wise to be able to close your program.
                   truth = False
               elif event.type == pygame.KEYDOWN:
-                  if event.key == pygame.K_w:
-                      FroggerY += 16 # :P
-                  elif event.key == pygame.K_a:
-                      FroggerX -= 16 # :P
-                  elif event.key == pygame.K_s:
-                      FroggerY -= 16 # :P
-                  elif event.key == pygame.K_d:
-                      FroggerX += 16 # :P
-        
-       drawScreen()
-    
+                  if event.key == pygame.K_UP and FroggerY > 0:
+                      FroggerY -= 64
+                  elif event.key == pygame.K_LEFT and FroggerX > 0:
+                      FroggerX -= 64
+                  elif event.key == pygame.K_DOWN and FroggerY < ScreenHeight - FroggerHeight * 2:
+                      FroggerY += 64
+                  elif event.key == pygame.K_RIGHT and FroggerX < ScreenWidth - FroggerWidth * 2:
+                      FroggerX += 64
+
+        drawScreen()
+
+        FroggerY += 1
+
+        if FroggerY >= ScreenHeight:
+            print("Game Over")
+            truth = False
+
+
     pygame.quit()
-             
-    
+
+
 
 main()
